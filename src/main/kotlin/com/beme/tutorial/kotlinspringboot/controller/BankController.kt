0b89afb2ab4697ac51454a206a7631e6f2d26d1d
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-05 19:46:31 +0900

    [FEAT] : Add post method in api

--- src/main/kotlin/com/beme/tutorial/kotlinspringboot/controller/BankController.kt
+++ src/main/kotlin/com/beme/tutorial/kotlinspringboot/controller/BankController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/banks")
 class BankController(private val bankService: BankService) {
     @ExceptionHandler(NoSuchElementException::class)
-    fun handleNotFound(e: NoSuchElementException) : ResponseEntity<String> =
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
         ResponseEntity(e.message, HttpStatus.NOT_FOUND)
 
     @GetMapping
@@ -18,4 +18,8 @@ class BankController(private val bankService: BankService) {
 
     @GetMapping("/{accountNumber}")
     fun getBank(@PathVariable accountNumber: String): Bank = bankService.getBank(accountNumber)
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerBank(@RequestBody bank: Bank): Bank = bank
 }
